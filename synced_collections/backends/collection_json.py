d95542ef17f1442fe3d02c0b460a33c029e10d4f
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2023-04-27 14:54:27 -0700

    Update copyrights

--- synced_collections/backends/collection_json.py
+++ synced_collections/backends/collection_json.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 The Regents of the University of Michigan
+# Copyright (c) 2023 The Regents of the University of Michigan
 # All rights reserved.
 # This software is licensed under the BSD 3-Clause License.
 """Implements a JSON :class:`~.SyncedCollection` backend."""
